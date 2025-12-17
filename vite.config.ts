25bd15a0aa88f2acb38cc42529a9708f08adeb75
MahmoudMohammed1024 <mm5070177@gmail.com>
2025-12-18 00:57:10 +0200

    Modify base path in vite.config.ts
    Updated the base path in the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tft-preview/',
+  base: '/tft-preview/', // 👈 مهم جدا
   plugins: [react()],
 })
